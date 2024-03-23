30dd9a4c83f80401d0dde13420e48257a50fa644
Darrryaa <159245758+Darrryaa@users.noreply.github.com>
2024-03-23 07:47:37 +0700

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,46 +1,21 @@
-p = input('Собака короткошерстной породы? ')
-n = input('Рост собаки менее 50 см? ')
-if p.lower() == 'да' and n.lower() == 'да':
-    h = input('У собаки короткий хвост? ')
-    if h.lower() == 'да':
-        print('английский бульдог')
-    else:
-        u = input('У собаки длинные уши? ')
-        if u.lower() == 'да':
-            print('гончая')
-        else:
-            t = input('У собаки короткое тело? ')
-            if t.lower() == 'да':
-                print('мопс')
-            else:
-                print('чихуахуа')
-elif p.lower() == 'да' and n.lower() == 'нет':
-    v = input('Собака весит более 50 кг? ')
-    if v.lower() == 'да':
-        print('датский дог')
-    else:
-        print('фоксхаунд')
-elif p.lower() == 'нет' and n.lower() == 'да':
-    d = input('У собаки доброжелательный характер? ')
-    if d.lower() == 'да':
-        print('кокер-спаниэль')
-    else:
-        print('ирландский сеттер')
-else:
-    r = input('У собаки рост менее 70 см? ')
-    if r.lower() == 'да':
-        l = input('У собаки длинные уши? ')
-        if l.lower() == 'да':
-            print('большой вандейский грифон')
-        else:
-            print('колли')
-    else:
-        o = input('Окрас рыжий с белыми отметинами? ')
-        if o.lower() == 'да':
-            print('сенбернар')
-        else:
-            x = input('Белоснежный окрас? ')
-            if x.lower() == 'да':
-                print('ирландский волкодав')
-            else:
-                print('ньюфаундленд')
+a = list(map(int, input().split()))
+castle_1 = a[0]
+castle_2 = a[1]
+castle_3 = a[2]
+castle = 0
+if castle_1 < castle_2:
+    castle = castle_1
+    castle_1 = castle_2
+    castle_2 = castle
+    castle = 0
+if castle_1 < castle_3:
+    castle = castle_1
+    castle_1 = castle_3
+    castle_3 = castle
+    castle = 0
+if castle_2 < castle_3:
+    castle = castle_2
+    castle_2 = castle_3
+    castle_3 = castle
+    castle = 0
+print(castle_1, castle_2, castle_3)
